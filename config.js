890d9212ee1f85f0f5fc47b15527e97c7bf39d50
= <aryamanaranya@gmail.com>
2021-06-10 19:36:42 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '592807295033342',
+        clientSecret: '9358619b7d6e5ce78ec4eed227a9b594'
+    }
 }
